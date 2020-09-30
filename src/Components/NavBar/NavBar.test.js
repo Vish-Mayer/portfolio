edf13bc2813c80a8f5433cc8f38494f07a60d322
Vish Mayer <vish.mayer@gmail.com>
2020-09-30 15:04:50 +0100

    updating route tests

--- src/Components/NavBar/NavBar.test.js
+++ src/Components/NavBar/NavBar.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBar from './NavBar';
+import { HashLink as Link } from 'react-router-hash-link';
 
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
@@ -14,19 +15,42 @@ describe('NavBar', () => {
     wrapper = shallow(<NavBar/>)
   });
 
-  it('renders a tab with text "Home"', () => {
-    expect(wrapper.find(".home").props().children).toBe("Home")
+  describe('Home', () => {
+    it('renders a tab with text "Home"', () => {
+      expect(wrapper.find(".home").props().children).toBe("Home")
+    })
+    it('navigates the user to the home screen', () => {
+      const link = Link
+      expect(wrapper.find(".home").props().to).toBe('/#home')
+    })
   })
 
-  it('renders a tab with text "About"', () => {
-    expect(wrapper.find(".about").props().children).toBe("About")
+  describe('Home', () => {
+    it('renders a tab with text "About"', () => {
+      expect(wrapper.find(".about").props().children).toBe("About")
+    })
+    it('navigates the user to about section', () => {
+      const link = Link
+      expect(wrapper.find(".about").props().to).toBe('/#about')
+    })
   })
 
-  it('renders a tab with text "Projects"', () => {
-    expect(wrapper.find(".projects").props().children).toBe("Projects")
+  describe('Home', () => {
+    it('renders a tab with text "Projects"', () => {
+      expect(wrapper.find(".projects").props().children).toBe("Projects")
+    })
+    it('navigates the user to the projects page/section', () => {
+      expect(wrapper.find(".projects").props().to).toBe("/projects/#project-content")
+    })
   })
 
-  it('renders a tab with text "Contact"', () => {
-    expect(wrapper.find(".contact").props().children).toBe("Contact")
+  describe('Home', () => {
+    it('renders a tab with text "Contact"', () => {
+      expect(wrapper.find(".contact").props().children).toBe("Contact")
+    })
+    it('navigates the user to the contact section', () => {
+      const link = Link
+      expect(wrapper.find(".contact").props().to).toBe('/#contact')
+    })
   })
 });
