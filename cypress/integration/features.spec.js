239c006612652e616b11a56c6c91e3bfa582695b
Vish Mayer <vish.mayer@gmail.com>
2020-10-01 17:30:56 +0100

    updated e2e testing

--- cypress/integration/features.spec.js
+++ cypress/integration/features.spec.js
@@ -1,10 +1,49 @@
-describe('Features', () => {
+import React from 'react'
 
-  it('it redirects the user to the projects page', () => {
-   
+describe('Features', () => {
+  
+
+  const redirectFromHome = (to) => {
+    cy.visit('http://localhost:3000/')
+    cy.get(to)
+    .click()
+  }
+
+  const redirectFromProjects = (to) => {
+    cy.visit('http://localhost:3000/projects/#project-content')
+    cy.get(to)
+    .click()
+  }
+
+  it('it directs the user to the about section', () => {
+    redirectFromHome('.about')
+    cy.url().should('eq', 'http://localhost:3000/#about')
+  })
+
+  it('it directs the user to the contact section', () => {
+    redirectFromHome('.contact')
+    cy.url().should('eq', 'http://localhost:3000/#contact')
+  })
+
+  it('it directs the user to the projects page', () => {
     cy.visit('http://localhost:3000')
     cy.get('.projects')
     .click()
     cy.url().should('eq', 'http://localhost:3000/projects/#project-content')
+  })
+
+  it('it redirects the user from the projects page to the home page', () => {
+    redirectFromProjects('.home')
+    cy.url().should('eq', 'http://localhost:3000/#home')
+  })
+
+  it('it redirects the user from the projects page to the about section', () => {
+    redirectFromProjects('.about')
+    cy.url().should('eq', 'http://localhost:3000/#about')
+  })
+
+  it('it redirects the user from the projects page to the contact section', () => {
+    redirectFromProjects('.contact')
+    cy.url().should('eq', 'http://localhost:3000/#contact')
   })
 })
