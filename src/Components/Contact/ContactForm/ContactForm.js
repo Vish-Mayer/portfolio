8ea6a17088982c083444ab88a6b0095d8e1f65b6
Vish Mayer <vish.mayer@gmail.com>
2020-10-02 17:43:38 +0100

    refactored tests

--- src/Components/Contact/ContactForm/ContactForm.js
+++ src/Components/Contact/ContactForm/ContactForm.js
@@ -8,30 +8,61 @@ class ContactForm extends Component{
     super(props)
     this.state={
       message:{
-        input: ''
+        name: '',
+        email: '',
+        body: '',
       }
     }
-    this.handleInput = this.handleInput.bind(this);
+    this.handleInput1 = this.handleInput1.bind(this);
+    this.handleInput2 = this.handleInput2.bind(this);
+    this.handleInput3 = this.handleInput3.bind(this);
+    this.sendEmail = this.sendEmail.bind(this)
   }
 
-  handleInput(e) {
+  handleInput1(e) {
     this.setState ({
       message:{
-        input: e.target.value 
+        name: e.target.value 
+      }
+    })
+  }
+
+  handleInput2(e) {
+    this.setState ({
+      message:{
+        email: e.target.value
+      }
+    })
+  }
+
+  handleInput3(e) {
+    this.setState ({
+      message:{
+        body: e.target.value
       }
     })
   }
 
   sendEmail(e){
     e.preventDefault();
-  
-    emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, e.target, process.env.REACT_APP_USER_ID)
+    emailjs.sendForm(
+      process.env.REACT_APP_SERVICE_ID,
+      process.env.REACT_APP_TEMPLATE_ID,
+      e.target, 
+      process.env.REACT_APP_USER_ID)
     .then((result) => {
         console.log(result.text);
     }, (error) => {
         console.log(error.text);
     });
-    e.target.reset()
+      e.target.reset()
+      this.setState({
+        message:{
+          name: '',
+          email: '',
+          body: '',
+        }
+      })
   }
 
   render() {
@@ -41,15 +72,32 @@ class ContactForm extends Component{
         <h4>Get in Touch!</h4>
         <div class="row">
           <div class="col">
-            <input value={this.state.message.input} onChange={this.handleInput} type="text" class="form-control" id="inputName" placeholder="Name" name="user_name"/>
+            <input 
+            value={this.state.message.name} 
+            onChange={this.handleInput1} 
+            type="text" class="form-control" 
+            id="inputName" placeholder="Name" 
+            name="user_name"/>
           </div>
         </div><br/>
         <div class="form-group">
-           <input value={this.state.message.input} onChange={this.handleInput} type="email" class="form-control" id="inputEmail" placeholder="Email address" name="user_email"/>
+           <input
+            value={this.state.message.email} 
+            onChange={this.handleInput2} 
+            type="email" class="form-control"
+            id="inputEmail" 
+            placeholder="Email address" 
+            name="user_email"/>
            </div>
            <div class="form-group">
             <label id ="input-message" for="FormTextarea">Your Message</label>
-            <textarea class="form-control" id="FormTextarea" name="message" cols ="70" rows="10"></textarea>
+            <textarea 
+            value={this.state.message.body} 
+            onChange={this.handleInput3} class="form-control" 
+            id="FormTextarea"
+            name="message" 
+            cols ="70" 
+            rows="10"></textarea>
           </div>
           <button type="submit" class="btn btn-primary" id="send">Send</button>
         </form>
--- src/Components/Contact/ContactForm/ContactForm.test.js
+++ src/Components/Contact/ContactForm/ContactForm.test.js
@@ -14,48 +14,51 @@ describe('ContactForm', () => {
     wrapper = shallow(<ContactForm/>)
   });
 
+  const simulateChange = (wrapper, inputSelector, newValue) => {
+    const input = wrapper.find(inputSelector)
+      input.simulate('change', {
+        target: {value: newValue}
+      })
+    return wrapper.find(inputSelector)
+  }
+
   it('displays header for email component', () => {
     expect(wrapper.find('h4').text()).toEqual('Get in Touch!')
   });
 
-  describe('inputName', () => {
-    it('has an input for a user to insert their name', () => {
-      const nameinput = wrapper.find('#inputName')
-      expect(nameinput.props().placeholder).toEqual("Name")
-    })
-    it('saves the name to current state', () => {
-      const input = wrapper.find('#inputName')
-      input.simulate('change', {
-        target: {value: "john doe"}
-      })
-      const newinput= wrapper.find('#inputName')
-      expect(newinput.props().value).toEqual("john doe")
-    })
-    
+  it('has a button for a user to send a message', () => {
+    expect(wrapper.find('#send').text()).toBe("Send")
   })
 
+  describe('input name', () => {
+    it('has an input for a user to insert their name', () => {
+      const input = wrapper.find('#inputName')
+      expect(input.props().placeholder).toEqual("Name")
+    })
+    it('saves the name to the current state', () => {
+      const updateInput = simulateChange(wrapper, '#inputName', "test name")
+      expect(updateInput.props().value).toEqual("test name")
+    })  
+  })
 
-  describe('inputEmail', () => {
+  describe('input email', () => {
     it('has an input for a user to insert their email', () => {
       const input = wrapper.find('#inputEmail')
       expect(input.props().placeholder).toEqual("Email address")
     })
-    it('saves the name to current state', () => {
-      const input = wrapper.find('#inputEmail')
-      input.simulate('change', {
-        target: {value: "test@test.com"}
-      })
-      const newinput= wrapper.find('#inputEmail')
-      expect(newinput.props().value).toEqual("test@test.com")
+    it('saves the email to the current state', () => {
+      const updateInput = simulateChange(wrapper, '#inputEmail', "test@test.com")
+      expect(updateInput.props().value).toEqual("test@test.com")
+    })   
+  })
+
+  describe('input message', () => {
+    it('has an input for a user to insert a message', () => {
+      expect(wrapper.find('#input-message').text()).toBe("Your Message")
     })
-    
-  })
-
-  it('has an input for a user to insert a message', () => {
-    expect(wrapper.find('#input-message').text()).toBe("Your Message")
-  })
-
-  it('has a button for a user to send a message', () => {
-    expect(wrapper.find('#send').text()).toBe("Send")
+    it('saves the message to the current state', () => {
+      const updateInput = simulateChange(wrapper, '#FormTextarea', "Test Message")
+      expect(updateInput.props().value).toEqual("Test Message")
+    })    
   })
 });
