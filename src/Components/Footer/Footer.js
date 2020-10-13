be5da6b350fdce5885061fc6068ddbbed87804dd
Vish Mayer <vish.mayer@gmail.com>
2020-10-13 13:25:16 +0100

    footer structure

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,8 +3,23 @@ import './Footer.css'
 
 function Footer() {
   return(
-    <div className="footer" id="footer">
-      This is a footer 
+    <div className="main-footer">
+      <div className="container-fluid p-0 d-flex justify-content-center">
+        <div className="row p-0">
+          <div className="col-md-3 col-sm-5">
+            <h1>Link</h1>
+          </div>
+          <div className="col-md-3 col-sm-5">
+            <h1>Link</h1>
+          </div>
+          <div className="col-md-3 col-sm-5">
+            <h1>Link</h1>
+          </div>
+          <div className="col-md-3 col-sm-5">
+            <h1>Link</h1>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
